d6e351eb6198f7e1b635092b8ac6617904e3199e
Débora <amaral.psic17@gmail.com>
2025-02-03 08:00:40 -0400

    removido de db.js useUnifiedTopology e useNewUrlParser e funciona normal o projeto

--- src/database/db.js
+++ src/database/db.js
@@ -4,9 +4,7 @@ const connectDataBase = () => {
     console.log("Espere estamos concectando ao Banco de Dados")
     
         mongoose
-        .connect(process.env.MONGODB_URI,
-            { useNewUrlParser: true, useUnifiedTopology: true }
-        )
+        .connect(process.env.MONGODB_URI)
         .then(() => console.log("MongoDB Atlas Conectado !"))
         .catch((error) => console.log(error))
 }
